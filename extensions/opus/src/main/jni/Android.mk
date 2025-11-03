abef9bc3fd5c9f4a552193bd6601541dd48efd96
Tim Malseed <t.malseed@gmail.com>
2025-11-03 21:47:36 +1100

    Add Android 16KB page size support to FLAC and Opus extensions
    Add linker flag -Wl,-z,max-page-size=16384 to native builds to ensure compatibility with Android 15 devices using 16KB page size

--- extensions/opus/src/main/jni/Android.mk
+++ extensions/opus/src/main/jni/Android.mk
@@ -28,6 +28,7 @@ LOCAL_MODULE := libopusV2JNI
 LOCAL_ARM_MODE := arm
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := opus_jni.cc
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 LOCAL_LDLIBS := -llog -lz -lm
 LOCAL_STATIC_LIBRARIES := libopus
 include $(BUILD_SHARED_LIBRARY)
